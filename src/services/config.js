adecfc577e627fbd13b22bb8b5cc5b49ffa24fb9
Vishal Patel <85049843+venteury@users.noreply.github.com>
2024-05-07 12:21:53 +0530

    config

---
+++ src/services/config.js
@@ -0,0 +1,11 @@
+const header1 = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+const config = {
+  header1,
+};
+
+export default config;
--- src/services/document.service.js
+++ src/services/document.service.js
@@ -1,25 +1,26 @@
 import axios from "axios";
+import config from "./config";
 let baseURL = "https://mydocsbackend-cnnn.onrender.com";
 
 const createDocument = (data) => {
   console.log(data);
-  return axios.post(baseURL + "/api/documents", data);
+  return axios.post(baseURL + "/api/documents", data, config);
 };
 
 const getDocumentById = (id) => {
-  return axios.get(baseURL + `/api/documents/${id}`);
+  return axios.get(baseURL + `/api/documents/${id}`, config);
 };
 
 const updateDocumentById = (id, data) => {
-  return axios.put(baseURL + `/api/documents/${id}`, data);
+  return axios.put(baseURL + `/api/documents/${id}`, data, config);
 };
 
 const deleteDocumentByID = (id) => {
-  return axios.delete(baseURL + `/api/documents/${id}`);
+  return axios.delete(baseURL + `/api/documents/${id}`, config);
 };
 
 const getAllDocuments = () => {
-  return axios.get(baseURL + "/api/documents");
+  return axios.get(baseURL + "/api/documents", config);
 };
 
 const documentServices = {
